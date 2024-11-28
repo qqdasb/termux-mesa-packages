fe1b25f62712ff9f97c44e72a78041fce44dc002
qqdasb <congminda188@outlook.com>
2024-11-28 22:03:02 +0800

    1

--- packages/libwayland-protocols/build.sh
+++ packages/libwayland-protocols/build.sh
@@ -5,7 +5,6 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.38"
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${TERMUX_PKG_VERSION}/downloads/wayland-protocols-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ff17292c05159d2b20ce6cacfe42d7e31a28198fa1429a769b03af7c38581dbe
-TERMUX_PKG_BUILD_DEPENDS="libwayland-cross-scanner"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=false
 "
