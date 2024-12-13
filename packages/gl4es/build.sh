085c5777d3a57d45379dfa5697fd229f54d0ff75
qqdasb <congminda188@outlook.com>
2024-12-13 20:17:43 +0800

    Added gl4es

---
+++ packages/gl4es/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://ptitseb.github.io/gl4es/
+TERMUX_PKG_DESCRIPTION="OpenGL driver for GLES devices"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.1.4"
+TERMUX_PKG_SRCURL=git+https://github.com/ptitSeb/gl4es
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="libx11"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DTERMUX=ON
+-DCMAKE_SYSTEM_NAME=Linux
+"
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout "${_COMMIT}"
+}
+
+termux_step_pre_configure() {
+	# benchmark result as follows:
+	# -O2 -flto > -O3 -flto > -O2 > -Os > -Os -flto > -O3 > -Oz > -Oz -flto
+	export CFLAGS="${CFLAGS/-Oz/-O2} -flto"
+}
+
+termux_step_post_make_install() {
+	rm -fr "${TERMUX_PREFIX}/lib/gl4es/libGL.so"
+	ln -fs "libGL.so.1" "${TERMUX_PREFIX}/lib/gl4es/libGL.so"
+}
