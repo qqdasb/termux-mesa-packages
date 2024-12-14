2069b71627b93152fc01bba406d2b1088f4551d0
qqdasb <congminda188@outlook.com>
2024-12-14 13:22:33 +0800

    rename

--- packages/mesa/build.sh
+++ packages/mesa/build.sh
@@ -10,7 +10,7 @@ _LLVM_MAJOR_VERSION_NEXT=$((_LLVM_MAJOR_VERSION + 1))
 TERMUX_PKG_SRCURL=git+https://gitlab.freedesktop.org/mesa/mesa.git
 TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libglvnd, libllvm (<< ${_LLVM_MAJOR_VERSION_NEXT}), libwayland, libx11, libxext, libxfixes, libxshmfence, libxxf86vm, ncurses, vulkan-loader, zlib, zstd, rust"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libglvnd, libllvm (<< ${_LLVM_MAJOR_VERSION_NEXT}), libwayland, libx11, libxext, libxfixes, libxshmfence, libxxf86vm, ncurses, vulkan-loader, zlib, zstd"
 TERMUX_PKG_SUGGESTS="mesa-dev"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols (<= 1.38), libxrandr, llvm, llvm-tools, mlir, xorgproto, rustc-dev, rust-bindgen"
 TERMUX_PKG_CONFLICTS="libmesa, ndk-sysroot (<= 25b)"
